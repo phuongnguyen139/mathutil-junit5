c1e5f65546ab0f64ba00c7ff568397d6e23ce53d
phuongnguyen139 <bapcaisach@gmail.com>
2023-10-17 21:43:36 +0700

    1st upload with maven project

---
+++ src/main/java/com/nvxp/mathutil/main/Main.java
@@ -0,0 +1,10 @@
+
+package com.nvxp.mathutil.main;
+
+import com.nvxp.mathutil.core.MathUtil;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("5! = " + MathUtil.getFactorial(5));
+    }
+}
---
+++ src/test/java/com/nvxp/mathutil/core/MathUtilTest.java
@@ -0,0 +1,62 @@
+
+package com.nvxp.mathutil.core;
+
+//import static com.nvxp.mathutil.core.MathUtil.getFactorial;
+import static com.nvxp.mathutil.core.MathUtil.*;
+//Import static là dành cho những hàm static, gọi hàm bỏ qua Class
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class MathUtilTest {
+    
+    //Chơi DDT, tách data
+    
+    //Chuẩn bị data
+    public static Object[][] initData() {
+        return new Object[][] {
+                {1, 1},
+                {2, 2},
+                {3, 6},
+                {4, 24},
+                {5, 120},
+                {60, 720}
+        };
+    }
+    
+    @ParameterizedTest
+    @MethodSource(value = "initData")  //tên hàm cung cấp data và ngầm định thứ tự của mảng.
+                                       //và map vào tham số hàm
+    public void testGetFactorialGivenRightArgReturnsWell(int input, long expected) {
+        assertEquals(expected, getFactorial(input));
+    }
+    
+//    @Test
+//    public void testGetFactorialGivenRightArgReturnsWell() {
+//        assertEquals(24, getFactorial(4));
+//    }
+    
+    //Bắt ngoại lệ khi đưa data cà chớn
+//    @Test (expected = tên ngoại lệ.class) -> JUnit 4 thôi
+    @Test
+    public void testGetFactorialGivenWrongArgThrowException() {
+        //Xài biểu thức lambda
+        //Hàm nhận tham số thứ 2 là 1 cái object - có code implement functional interface 
+        // tên là Executable - có 1 hàm duy nhất không code
+        
+        //Chơi chậm
+//         Executable exceptionObj = new Executable() {
+//             @Override
+//             public void execute() throws Throwable {
+//                 getFactorial(-5);
+//             }
+//         };
+//        assertThrows(IllegalArgumentException.class, exceptionObj);
+        
+//        Executable exceptionObj = () -> getFactorial(-5);
+
+        //Chơi nhanh
+        assertThrows(IllegalArgumentException.class, () -> getFactorial(-5));
+    }
+}
